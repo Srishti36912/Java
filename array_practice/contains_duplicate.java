1e374729853e9721c84e05a4fedc5a9a32b2a089
Srishti <shrishtitripathi43@gmail.com>
2024-09-11 22:50:00 +0530

    Array duplicate ques

---
+++ array_practice/contains_duplicate.java
@@ -0,0 +1,30 @@
+package array_practice;
+
+import java.util.*;
+
+public class contains_duplicate {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int array[] = new int[5];
+        int m = 0;
+        for(int i=0; i<5; i++) {
+            array[i] = sc.nextInt();
+        }
+        while(m<5) {
+        for(int i=0; i<4; i++) {
+            if(m == i) {
+                continue;
+            }
+            else
+            {
+                if(array[m] == array[i]) {
+                    System.out.println("Contains Duplicate");
+                    break;
+                }
+            }
+        }
+        m++;
+        }
+        System.out.println("Does not contain duplicate");
+    }
+}
