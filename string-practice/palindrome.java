a5501c9f88cbe103c1d030b487607ac472026e26
Srishti <shrishtitripathi43@gmail.com>
2024-09-16 19:13:52 +0530

    error resolve

--- string-practice/palindrome.java
+++ string-practice/palindrome.java
@@ -2,11 +2,11 @@ import java.util.*;
 public class palindrome {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s="", rev="";
+        String s, rev="";
         System.out.println("Enter a string");
         s = sc.nextLine();
 
-        for(int i=0; i<s.length(); i--) {
+        for(int i=0; i<s.length(); i++) {
             char c = s.charAt(i);
             rev = c + rev;
         }
