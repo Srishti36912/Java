d72c256b29d22bcfc9ded6e19d3601523549cf2b
Srishti Tripathi <shrishtitripathi43@gmail.com>
2025-01-18 16:38:42 +0530

    error resolve

--- array_practice/subarray.java
+++ array_practice/subarray.java
@@ -1,3 +1,4 @@
+package array_practice;
 import java.util.*;
 public class subarray {
     public static void main(String args[]) {
---
+++ selection.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class selection {
+
+    int select(int arr[], int i)
+    {
+    // code here such that selecionSort() sorts arr[]
+    int min = i;
+    
+    for(int j=i+1; j < arr.length; j++) {
+        if(arr[j] < arr[min]) {
+          min = j;
+        }
+    }
+    return min;
+    }
+
+    void selectionSort(int arr[], int n)
+    {
+  //code here
+  int temp = 0;
+  for(int i=0; i<n-1; i++) {
+    int min = select(arr,i);
+      
+    temp = arr[i];
+    arr[i] = arr[min];
+    arr[min] = temp;
+    }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = {1,67,54,32,89};
+        int n = arr.length;
+        selectionSort(arr,n);
+
+        for(int i=0; i<arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
